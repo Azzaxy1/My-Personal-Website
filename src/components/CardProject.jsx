905cd81b39f63a6a628c00334f3738a8d6047586
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-13 22:56:35 +0700

    feat: add new project

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,155 +1,42 @@
 import { Link } from "react-router-dom";
-import htmlIcons from "../assets/icons/html.png";
-import bootstrapIcons from "../assets/bootstrap.png";
-import tailwindIcons from "../assets/tailwind.png";
-import reactIcons from "../assets/react.png";
-import reduxIcons from "../assets/icons/redux.png";
 
-import project1 from "../assets/img-project/project1.jpeg";
-import KKM01 from "../assets/img-project/kkm01.png";
-import project2 from "../assets/img-project/project2.jpeg";
-import project3 from "../assets/img-project/project3.png";
-import project4 from "../assets/img-project/last-king.png";
+import { projects } from "@/utils";
 
 const CardProject = () => {
   return (
     <>
-      <div
-        data-aos="fade-right"
-        data-aos-easing="ease-in-sine"
-        className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md cursor-pointer lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
-      >
-        <Link to="https://kkm01-desa-panyabrangan.vercel.app/">
-          <img
-            src={KKM01}
-            alt="Project1"
-            className="p-3 border-gray-300 rounded-2xl"
-          />
-        </Link>
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold">Rental Car</h1>
-          <div className="flex items-center gap-3">
-            <p className="text-base text-gray-700">Landing Page •</p>
-            <img src={htmlIcons} width={30} alt="" />
-            <img src={bootstrapIcons} width={30} alt="" />
-          </div>
-          <Link to="https://github.com/Azzaxy1/Ch1-RentalCar-Botstrapp">
-            <p className="mt-3 text-base underline decoration-wavy text-primary">
-              Link Github
-            </p>
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          data-aos="fade-right"
+          data-aos-easing="ease-in-sine"
+          className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
+        >
+          <Link to={project.liveLink} target="_blank">
+            <img
+              src={project.image}
+              alt="Project1"
+              className="p-3 border-gray-300 cursor-pointer rounded-2xl"
+            />
           </Link>
-        </div>
-      </div>
-      <div
-        data-aos="fade-right"
-        data-aos-easing="ease-in-sine"
-        className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md cursor-pointer lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
-      >
-        <Link to="https://rentalcar-azis.vercel.app/">
-          <img
-            src={project1}
-            alt="Project1"
-            className="p-3 border-gray-300 rounded-2xl"
-          />
-        </Link>
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold">Rental Car</h1>
-          <div className="flex items-center gap-3">
-            <p className="text-base text-gray-700">Landing Page •</p>
-            <img src={htmlIcons} width={30} alt="" />
-            <img src={bootstrapIcons} width={30} alt="" />
-          </div>
-          <Link to="https://github.com/Azzaxy1/Ch1-RentalCar-Botstrapp">
-            <p className="mt-3 text-base underline decoration-wavy text-primary">
-              Link Github
-            </p>
-          </Link>
-        </div>
-      </div>
-      <div
-        data-aos="fade-left"
-        data-aos-easing="ease-in-sine"
-        className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md cursor-pointer lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
-      >
-        <Link to="https://todo-list-app-azis.vercel.app/">
-          <img
-            src={project2}
-            alt="Project2"
-            className="p-3 border-gray-300 rounded-2xl"
-          />
-        </Link>
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold">Todo List</h1>
-          <div className="flex items-center gap-3">
-            <p className="text-base text-gray-700">Todo List App •</p>
-            <img src={reactIcons} width={30} alt="" />
-            <img src={tailwindIcons} width={30} alt="" />
-          </div>
-          <Link to="https://github.com/Azzaxy1/Ch3-TodoList-App-React">
-            <p className="mt-3 text-base underline decoration-wavy text-primary">
-              Link Github
-            </p>
-          </Link>
-        </div>
-      </div>
-      <div
-        data-aos="fade-right"
-        data-aos-easing="ease-in-sine"
-        className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md cursor-pointer lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
-      >
-        <Link to="https://project-6-team-6-redux.vercel.app/id">
-          <img
-            src={project3}
-            alt="Project3"
-            className="p-3 border-gray-300 rounded-2xl"
-          />
-        </Link>
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold text-red-500">MOVIELIST</h1>
-          <div className="flex items-center gap-3">
-            <p className="text-base text-gray-700">Movie Rating Film •</p>
-            <img src={reactIcons} width={30} alt="" />
-            <img src={tailwindIcons} width={30} alt="" />
-            <img src={reduxIcons} width={30} alt="" />
-          </div>
-          <Link to="https://github.com/Azzaxy1/Ch7-MovieApp-React-Redux">
-            <p className="mt-3 text-base underline decoration-wavy text-primary">
-              Link Github
-            </p>
-          </Link>
-        </div>
-      </div>
-      <div
-        data-aos="fade-left"
-        data-aos-easing="ease-in-sine"
-        className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md cursor-pointer lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
-      >
-        <Link to="https://last-king-academy.vercel.app/">
-          <img
-            src={project4}
-            alt="Project4"
-            className="p-3 border-gray-300 rounded-2xl"
-          />
-        </Link>
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold">Last King Academy</h1>
-          <div className="flex items-center gap-3">
-            <p className="text-base text-gray-700">Web Course •</p>
-            <img src={reactIcons} width={30} alt="" />
-            <img src={tailwindIcons} width={30} alt="" />
-            <img src={reduxIcons} width={30} alt="" />
-          </div>
-          <div className="flex items-center justify-between">
-            <Link
-              to="https://github.com/Azzaxy1/Last-King-Academy"
-              className="mt-3 text-base underline decoration-wavy text-primary"
-            >
-              {" "}
-              Link Github
+          <div className="px-4 py-4 border-t-4">
+            <h1 className="text-xl font-bold">{project.name}</h1>
+            <div className="flex items-center gap-3">
+              <p className="text-base text-gray-700">{project.description} •</p>
+              {project.technologies.map((tech, index) => (
+                <div key={index} className="relative group">
+                  <img src={tech} width={25} alt={project.name} />
+                </div>
+              ))}
+            </div>
+            <Link to={project.githubLink} target="_blank">
+              <p className="mt-3 text-base underline decoration-wavy text-primary">
+                Link Github
+              </p>
             </Link>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,10 @@
+import htmlIcons from "../assets/icons/html.png";
+import cssIcons from "../assets/icons/css.svg";
 import jsIcons from "../assets/icons/javascript.svg";
 import bootstrapIcons from "../assets/icons/bootstrap.svg";
 import tailwindIcons from "../assets/icons/tailwind.svg";
 import reactIcons from "../assets/icons/react.svg";
+import react from "../assets/react.png";
 import reduxIcons from "../assets/icons/redux.svg";
 import postmanIcons from "../assets/icons/postman.svg";
 import figmaIcons from "../assets/icons/figma.svg";
@@ -10,9 +13,21 @@ import TSIcons from "../assets/icons/typescript.svg";
 import NextIcons from "../assets/icons/next-js.png";
 import NodeIcons from "../assets/icons/node-js.svg";
 import ExpressIcons from "../assets/icons/express.svg";
+import NextUIIcon from "../assets/icons/next-ui.png";
+import CypressIcon from "../assets/icons/cypress.webp";
 
 import BinarLogo from "../assets/binar-logo.png";
 import DicodingLogo from "../assets/dicoding.webp";
+
+import project1 from "../assets/img-project/project1.jpeg";
+import KKM01 from "../assets/img-project/kkm01.png";
+import DiscusZone from "../assets/img-project/discus-zone.png";
+import PersonalWeb from "../assets/img-project/personal-web.png";
+import NotesHive from "../assets/img-project/note-hive.png";
+import JobFinder from "../assets/img-project/job-finder.png";
+import project2 from "../assets/img-project/project2.jpeg";
+import project3 from "../assets/img-project/project3.png";
+import project4 from "../assets/img-project/last-king.png";
 
 const icons = [
   { id: 1, image: jsIcons, name: "JavaScript" },
@@ -27,6 +42,90 @@ const icons = [
   { id: 10, image: postmanIcons, name: "Postman" },
   { id: 11, image: figmaIcons, name: "Figma" },
   { id: 12, image: npmIcons, name: "npm" },
+];
+
+const projects = [
+  {
+    id: 1,
+    name: "Job Finder",
+    image: JobFinder,
+    description: "Job Portal",
+    technologies: [react, NextUIIcon, reduxIcons],
+    liveLink: "https://job-finder-beige.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/fe-job-finder",
+  },
+  {
+    id: 2,
+    name: "Last King Academy",
+    image: project4,
+    description: "Web Course",
+    technologies: [react, tailwindIcons, reduxIcons],
+    liveLink: "https://last-king-academy.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/Last-King-Academy",
+  },
+  {
+    id: 3,
+    name: "KKM 01 Desa Panyabrangan",
+    image: KKM01,
+    description: "Landing Page",
+    technologies: [NextIcons, NextUIIcon, TSIcons],
+    liveLink: "https://kkm01-desa-panyabrangan.vercel.app/",
+    githubLink: "https://github.com/azzaxy1/kkm01-profile",
+  },
+  {
+    id: 4,
+    name: "MOVIELIST",
+    image: project3,
+    description: "Movie Rating Film",
+    technologies: [react, tailwindIcons, reduxIcons],
+    liveLink: "https://project-6-team-6-redux.vercel.app/id",
+    githubLink: "https://github.com/Azzaxy1/Ch7-MovieApp-React-Redux",
+  },
+  {
+    id: 5,
+    name: "Discus Zone",
+    image: DiscusZone,
+    description: "Discus App",
+    technologies: [react, tailwindIcons, reduxIcons, CypressIcon],
+    liveLink: "https://discus-zone.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/discus-zone",
+  },
+  {
+    id: 6,
+    name: "Notes Hive",
+    image: NotesHive,
+    description: "Notes App",
+    technologies: [react, tailwindIcons],
+    liveLink: "https://notes-hive.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/notes-hive",
+  },
+  {
+    id: 7,
+    name: "Todo List",
+    image: project2,
+    description: "Todo List App",
+    technologies: [react, tailwindIcons],
+    liveLink: "https://todo-list-app-azis.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/Ch3-TodoList-App-React",
+  },
+  {
+    id: 8,
+    name: "Abdurrohman Azis",
+    image: PersonalWeb,
+    description: "Personal Website",
+    technologies: [htmlIcons, cssIcons],
+    liveLink: "https://azis-personal-website.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/Belajar-Dasar-Pemrograman-Web",
+  },
+  {
+    id: 9,
+    name: "Rental Car",
+    image: project1,
+    description: "Landing Page",
+    technologies: [htmlIcons, bootstrapIcons],
+    liveLink: "https://rentalcar-azis.vercel.app/",
+    githubLink: "https://github.com/Azzaxy1/Ch1-RentalCar-Botstrapp",
+  },
 ];
 
 const experience = [
@@ -59,4 +158,4 @@ const experience = [
   },
 ];
 
-export { experience, icons };
+export { experience, icons, projects };
