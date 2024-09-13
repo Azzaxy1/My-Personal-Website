eb1cd8abc6e7f2d9107495e54ffd7e06f99547d0
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-13 21:36:24 +0700

    fix: id array icons

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,3 @@
-import htmlIcons from "../assets/icons/html.svg";
-import cssIcons from "../assets/icons/css.svg";
 import jsIcons from "../assets/icons/javascript.svg";
 import bootstrapIcons from "../assets/icons/bootstrap.svg";
 import tailwindIcons from "../assets/icons/tailwind.svg";
@@ -7,9 +5,6 @@ import reactIcons from "../assets/icons/react.svg";
 import reduxIcons from "../assets/icons/redux.svg";
 import postmanIcons from "../assets/icons/postman.svg";
 import figmaIcons from "../assets/icons/figma.svg";
-import gitIcons from "../assets/icons/git.svg";
-import vsCodeIcons from "../assets/icons/vscode.svg";
-import githubIcons from "../assets/icons/github.svg";
 import npmIcons from "../assets/icons/npm.svg";
 import TSIcons from "../assets/icons/typescript.svg";
 import NextIcons from "../assets/icons/next-js.png";
@@ -20,23 +15,18 @@ import BinarLogo from "../assets/binar-logo.png";
 import DicodingLogo from "../assets/dicoding.webp";
 
 const icons = [
-  { id: 1, image: htmlIcons, name: "HTML" },
-  { id: 2, image: cssIcons, name: "CSS" },
-  { id: 3, image: jsIcons, name: "JavaScript" },
-  { id: 4, image: TSIcons, name: "TypeScript" },
-  { id: 5, image: bootstrapIcons, name: "Bootstrap" },
-  { id: 6, image: tailwindIcons, name: "Tailwind CSS" },
-  { id: 7, image: reactIcons, name: "React" },
-  { id: 8, image: reduxIcons, name: "Redux" },
-  { id: 9, image: NextIcons, name: "Next.js" },
-  { id: 10, image: NodeIcons, name: "Node.js" },
-  { id: 11, image: ExpressIcons, name: "Express.js" },
-  { id: 12, image: postmanIcons, name: "Postman" },
-  { id: 13, image: figmaIcons, name: "Figma" },
-  { id: 14, image: gitIcons, name: "Git" },
-  { id: 15, image: vsCodeIcons, name: "VS Code" },
-  { id: 16, image: githubIcons, name: "GitHub" },
-  { id: 17, image: npmIcons, name: "npm" },
+  { id: 1, image: jsIcons, name: "JavaScript" },
+  { id: 2, image: TSIcons, name: "TypeScript" },
+  { id: 3, image: bootstrapIcons, name: "Bootstrap" },
+  { id: 4, image: tailwindIcons, name: "Tailwind CSS" },
+  { id: 5, image: reactIcons, name: "React" },
+  { id: 6, image: reduxIcons, name: "Redux" },
+  { id: 7, image: NextIcons, name: "Next.js" },
+  { id: 8, image: NodeIcons, name: "Node.js" },
+  { id: 9, image: ExpressIcons, name: "Express.js" },
+  { id: 10, image: postmanIcons, name: "Postman" },
+  { id: 11, image: figmaIcons, name: "Figma" },
+  { id: 12, image: npmIcons, name: "npm" },
 ];
 
 const experience = [
