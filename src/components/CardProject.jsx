82fa983ba71bf6bf57093e10cb21a65663bb684b
Azzaxy <abdurrohmanazis@gmail.com>
2024-10-13 15:11:26 +0700

    style: adjust Tailwind classes in CardProject for better resposiveness

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -18,32 +18,34 @@ const CardProject = ({
         data-aos-easing="ease-in-sine"
         className="max-w-sm mx-6 my-6 overflow-hidden transition-all duration-300 rounded-lg shadow-md sm:max-w-lg md:max-w-xl lg:max-w-lg 2xl:max-w-xl hover:shadow-lg hover:scale-105 hover:ease-linear"
       >
-        <img
-          src={image}
-          alt={name}
-          className="p-3 border-gray-300 rounded-2xl"
-        />
-        <div className="px-4 py-4 border-t-4">
-          <h1 className="text-xl font-bold">{name}</h1>
-          <div className="flex items-center justify-between gap-3">
+        <header>
+          <img
+            src={image}
+            alt={name}
+            className="p-3 border-gray-300 rounded-2xl"
+          />
+        </header>
+        <footer className="flex justify-between px-4 py-4 border-t-4">
+          <div>
+            <h1 className="text-base font-bold md:text-xl">{name}</h1>
             <div className="flex gap-2">
-              <p className="text-base text-gray-700">{description} •</p>
+              <p className="text-sm text-gray-700">{description} •</p>
               {technologies.map((tech, index) => (
                 <div key={index} className="relative group">
                   <img src={tech} width={25} alt={name} />
                 </div>
               ))}
             </div>
-            <div className="flex flex-row items-center justify-end gap-3">
-              <Link to={githubLink} target="_blank">
-                <FaGithub className="w-[25px] h-[25px] md:w-[30px] md:h-[30px] duration-75 hover:text-primary hover:transform hover:-translate-y-1 hover:transition-all" />
-              </Link>
-              <Link to={liveLink} target="_blank">
-                <FaExternalLinkAlt className="w-[21px] h-[21px] duration-75 hover:transform hover:transition-all hover:-translate-y-1 text-[#357998] hover:text-primary" />
-              </Link>
-            </div>
           </div>
-        </div>
+          <div className="flex flex-row items-center justify-end gap-3">
+            <Link to={githubLink} target="_blank">
+              <FaGithub className="w-[25px] h-[25px] md:w-[30px] md:h-[30px] duration-75 hover:text-primary hover:transform hover:-translate-y-1 hover:transition-all" />
+            </Link>
+            <Link to={liveLink} target="_blank">
+              <FaExternalLinkAlt className="w-[21px] h-[21px] duration-75 hover:transform hover:transition-all hover:-translate-y-1 text-[#357998] hover:text-primary" />
+            </Link>
+          </div>
+        </footer>
       </div>
     </>
   );
